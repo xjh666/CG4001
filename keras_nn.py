8205655d8b9b27fc479e9ecd0e0c1fd5715cbeff
XieJihui <xjh125470939@gmail.com>
2019-03-14 17:09:55 +0800

    no message

--- keras_nn.py
+++ keras_nn.py
@@ -20,19 +20,50 @@ def load_model(model_name):
     model.load_weights(model_name + '_weights.h5')
     return model
 
-def run_ml(X_train, X_test, y_train, y_test, model_name):
+def run_ml(X_train, X_test, y_train, y_test, file_name, test_file_name):
     model = linear_model.LinearRegression()
-    # model = linear_model.Ridge(alpha=.5)
-    # model = linear_model.RidgeCV(alphas=[0.1, 1.0, 10.0], cv=3)
-    # model = linear_model.Lasso(alpha=0.1)
-    # model = linear_model.LassoLars(alpha=.1)
-    # model = linear_model.BayesianRidge()
-    # model = Pipeline([('poly', PolynomialFeatures(degree=3)), ('linear', linear_model.LinearRegression(fit_intercept=False))])
+    model.fit(X_train, y_train)
+    prediction = model.predict(X_test)
+    predict = numpy.append(y_test, prediction, axis=1)
+    print(len(predict[0]))
+    
+    model = linear_model.Ridge(alpha=.5)
+    model.fit(X_train, y_train)
+    prediction = model.predict(X_test)
+    predict = numpy.append(predict, prediction, axis=1)
+    print(len(predict[0]))
+
+    model = linear_model.RidgeCV(alphas=[0.1, 1.0, 10.0], cv=3)
+    model.fit(X_train, y_train)
+    prediction = model.predict(X_test)
+    predict = numpy.append(predict, prediction, axis=1)
+    print(len(predict[0]))
+
+    model = linear_model.Lasso(alpha=0.1)
+    model.fit(X_train, y_train)
+    prediction = numpy.reshape(model.predict(X_test), (-1, 1))
+    predict = numpy.append(predict, prediction, axis=1)
+    print(len(predict[0]))
+
+    model = linear_model.LassoLars(alpha=.1)
+    model.fit(X_train, y_train)
+    prediction = numpy.reshape(model.predict(X_test), (-1, 1))
+    predict = numpy.append(predict, prediction, axis=1)
+    print(len(predict[0]))
+    
+    model = linear_model.BayesianRidge()
+    model.fit(X_train, y_train)
+    prediction = numpy.reshape(model.predict(X_test), (-1, 1))
+    predict = numpy.append(predict, prediction, axis=1)
+    print(len(predict[0]))
+    
+    model = Pipeline([('poly', PolynomialFeatures(degree=3)), ('linear', linear_model.LinearRegression(fit_intercept=False))])
+    model.fit(X_train, y_train)
+    prediction = model.predict(X_test)
+    predict = numpy.append(predict, prediction, axis=1)
+    print(len(predict[0]))
+    
     # if(os.path.isfile(model_name + '.json')):
     #     model = load_model(model_name)
-
-    model.fit(X_train, y_train)
     # save_model(model, model_name)
-    prediction = model.predict(X_test)
-    predict = numpy.append(prediction, y_test, axis=1)
-    numpy.savetxt("predict.csv", predict, delimiter=",")
+    numpy.savetxt('/result/' + file_name + '_' + test_file_name, predict, delimiter=",")
--- regression.py
+++ regression.py
@@ -4,17 +4,19 @@ from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from keras_nn import run_ml
 
-file_name = 'Unity_frametime'
+file_name = 'unity_frametime'
 dataframe = pd.read_csv(file_name + '.csv', header=None, low_memory=False)
 dataset = dataframe.values
 X_train = dataset[1:,1:len(dataset[0])].astype(float)
 y_train = dataset[1:,0:1].astype(float)
 
-file_name = 'test'
-dataframe = pd.read_csv(file_name + '.csv', header=None, low_memory=False)
+test_file_name = 'effect_1'
+dataframe = pd.read_csv('test/' + file_name + '_' +test_file_name + '.csv', header=None, low_memory=False)
 dataset = dataframe.values
-X_test = dataset[1:,1:len(dataset[0])].astype(float)
-y_test = dataset[1:,0:1].astype(float)
+X_test = dataset[0:,1:len(dataset[0])].astype(float)
+y_test = dataset[0:,0:1].astype(float)
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.001)
 
-run_ml(X_train, X_test, y_train, y_test, file_name + '_mlmodel')
+print(X_test[0])
+print(X_train[0])
+run_ml(X_train, X_test, y_train, y_test, file_name, test_file_name)
